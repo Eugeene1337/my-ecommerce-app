2b6d828753aadb4d9d941dea802af58a9a26b9f9
Eugene1337 <zhenya777pinsk@gmail.com>
2022-05-20 19:01:11 +0200

    add and remove item from cart

--- core/models.py
+++ core/models.py
@@ -1,7 +1,8 @@
-from pickle import TRUE
-from unicodedata import category
+from django.db.models.signals import post_save
 from django.conf import settings
 from django.db import models
+from django.db.models import Sum
+from django.shortcuts import reverse
 
 CATEGORY_CHOICES = (
     ('S', 'Shirt'),
@@ -19,18 +20,40 @@ LABEL_CHOICES = (
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    slug = models.SlugField()
+    description = models.TextField()
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs={
+            'slug': self.slug
+        })
+
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
+    def get_remove_from_cart_url(self):
+        return reverse("core:remove-from-cart", kwargs={
+            'slug': self.slug
+        })
 
 
 class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.title
+        return f"{self.quantity} of {self.item.title}"
 
 
 class Order(models.Model):
@@ -42,4 +65,4 @@ class Order(models.Model):
     ordered = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.title
+        return self.user.username
